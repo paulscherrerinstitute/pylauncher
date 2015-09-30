f36e1d7813e56e01588cebe0fa26ae5d5fd324f2
Vintar Rok <rok.vintar@psi.ch>
2015-09-30 14:20:31 +0200

    COSYLAB-385 convert.py updated for command

--- src/convert/convert.py
+++ src/convert/convert.py
@@ -185,7 +185,7 @@ class LauncherMenuModel(object):
 
             element['type'] = 'cmd'
             element['text'] = params[0].replace('"', r'\"')
-            element['params'] = [cmd_text]
+            element['command'] = cmd_text
 
         # Add the element dictionary to the list of menu items
         if len(element) > 0:
