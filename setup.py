5352277a86781e52c3494ef369f5cf9acb26741c
Vintar Rok <rok.vintar@psi.ch>
2016-04-21 11:08:26 +0200

    version 1.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'Readme.md')).read()
 
 setup(name='pylauncher',
-      version='1.2.1',
+      version='1.2.2',
       description="Standard PSI tool for accessing GUIs",
       long_description=README,
       author='Rok Vintar, Cosylab d.d. for Paul Scherrer Institute (PSI)',
