aa53730398c3751b6c0a5a8c1c6a3a0a533f4d44
Simon Ebner <simon.ebner@psi.ch>
2020-03-10 11:38:53 +0100

    bump version and cleanup setup.py

--- setup.py
+++ setup.py
@@ -7,19 +7,11 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'Readme.md')).read()
 
 setup(name='pylauncher',
-      version='2.0.1',
+      version='2.0.2',
       description="Standard PSI tool for accessing GUIs",
       long_description=README,
-      author='Paul Scherrer Insitute',
-      classifiers=[
-          'Operating System :: MacOS :: MacOS X',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: POSIX',
-          'Programming Language :: Python:: 2.7',
-          'Programming Language :: Python:: 3',
-          ],
-      url='https://github.psi.ch/projects/COS/repos/pylauncher/browse',
-      keywords='launcher, caqtdm, medm, PSI',
+      author='Paul Scherrer Institute',
+      url='https://github.com/paulscherrerinstitute/pylauncher',
       packages=['pylauncher', 'pylauncher.convert'],
       package_dir={'pylauncher': 'src'},
       package_data={'pylauncher': ['resources/images/*.png', 'resources/qss/*.qss', 'resources/mapping/*.json']},
