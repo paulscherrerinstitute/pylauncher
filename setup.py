7d72b5aa73d524c5f24c3f963c0331a67d2e54e5
Simon Ebner <simon.ebner@psi.ch>
2020-03-11 07:41:14 +0100

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'Readme.md')).read()
 
 setup(name='pylauncher',
-      version='2.0.2',
+      version='2.0.3',
       description="Standard PSI tool for accessing GUIs",
       long_description=README,
       author='Paul Scherrer Institute',
